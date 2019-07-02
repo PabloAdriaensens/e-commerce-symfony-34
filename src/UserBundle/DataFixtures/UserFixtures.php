0925a1c78324622487edbf259ad3949e5265beeb
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-07-03 00:03:58 +0200

    Added faker to automate the user creation process

--- src/UserBundle/DataFixtures/UserFixtures.php
+++ src/UserBundle/DataFixtures/UserFixtures.php
@@ -5,19 +5,28 @@ namespace UserBundle\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use UserBundle\Entity\User;
+use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setName("PruebaFixture");
-        $user->setEmail("prueba@gmail.com");
-        $user->setPassword("123456");
-        $user->setAdmin("0");
-        $user->setCreatedAt(new \DateTime('now'));
-        $user->setUpdatedAt(new \DateTime('now'));
-        $manager->persist($user);
-        $manager->flush();
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setName($faker->firstNameMale());
+            $user->setEmail($faker->unique()->freeEmail);
+            $user->setPassword(sha1($faker->password));
+            if ($i === 0) {
+                $user->setAdmin(1);
+            } else {
+                $user->setAdmin(0);
+            }
+            $user->setCreatedAt(new \DateTime('now'));;
+            $user->setUpdatedAt(new \DateTime('now'));;
+            $manager->persist($user);
+            $manager->flush();
+        }
     }
 }
